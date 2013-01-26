0e03e960c17ba9af0e53c7998d62f1019079a53c
hit9 <nz2324@126.com>
2013-01-26 21:59:24 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="img2txt",
+    version="0.1",
+    author="hit9",
+    author_email="nz2324@126.com",
+    description=("Image to Ascii Text."),
+    license="BSD", 
+    url="https://github.com/hit9/img2txt",
+    install_requires = ['docopt'],
+    scripts=['img2txt']
+)
