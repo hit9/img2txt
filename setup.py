bfcdd8d87ce4fa9b02971ad4cd17465aeed95888
hit9 <hit9@icloud.com>
2015-11-15 13:04:42 +0800

    v2.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="img2txt.py",
-    version="2.0",
+    version="2.1",
     author="hit9",
     author_email="hit9@icloud.com",
     description="Image to Ascii Text, can output to html or ansi terminal.",
     license="BSD",
-    url="http://hit9.org/img2txt",
+    url="http://hit9.github.io/img2txt",
     install_requires=['docopt', 'Pillow'],
     scripts=['img2txt.py']
 )
